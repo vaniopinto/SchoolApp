dde0a8b929bc8e40cce5fc59ad9ba036f7c2a89d
Vânio Pinto <vauriovandunem@gmail.com>
2026-01-15 14:47:14 +0100

    feat: adding Semester model and migration

---
+++ app/Models/Semester.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Semester extends Model
+{
+    use HasUuids;
+
+    protected $fillable = [ "name", "start_at", "end_at" ];
+}
---
+++ database/migrations/2026_01_15_134300_create_semesters_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('semesters', function (Blueprint $table) {
+            $table->uuid('id')->unique()->primary();
+            $table->foreignUuid('academic_year_id')->constrained()->cascadeOnDelete();
+            $table->string('name'); // 1º Semestre
+            $table->date('starts_at');
+            $table->date('ends_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('semesters');
+    }
+};
